6fa07093b88f6f7ab5a7dae90276e62a943063ec
shreya datta <57366373+shreyadatta-AEIE@users.noreply.github.com>
2021-03-27 21:24:14 +0530

    profile-model added

--- model.py
+++ model.py
@@ -1,5 +1,11 @@
  from django.db import models
-  class student(models):
+ class Profile(models.Model):
+   user=models.CharField(max_length=30 , blank=False)
+  
+   def __str__(self):
+    return self.user
+  
+  class student(models.Model):
     name=models.CharField(max_length=50)
     age=models.IntegerField()
     contact=models.CharField(max_length=10)
