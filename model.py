9c3c5b1803f512c3063aebd49d16e050fae21c66
shreya datta <57366373+shreyadatta-AEIE@users.noreply.github.com>
2021-03-27 21:12:25 +0530

    department is add

--- model.py
+++ model.py
@@ -3,6 +3,7 @@
     name=models.CharField(max_length=50)
     age=models.IntegerField()
     contact=models.CharField(max_length=10)
+    department=models.CharField(max_length=5)
     
     def __str__(self):
       return self.name
